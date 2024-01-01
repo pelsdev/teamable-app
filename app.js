cc38b4a3cc3abf76c63bb0d2b05373e87ffb32d4
pelsdev <pelumiajiteru@gmail.com>
2024-01-01 22:39:53 +0100

    Complete edit functionality

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function handleEditProfile() {
+     document.getElementById("name-input").value = document.getElementById("name").textContent
+    
+     document.getElementById("email-input").value= document.getElementById("email").textContent
+    
+     document.getElementById("interest-input").value= document.getElementById("interest").textContent
+    
     document.getElementById("edit-profile").style.display="block"
+    
     document.getElementById("main").style.display="none"
 
 }
@@ -9,9 +16,9 @@ function handleUpdateProfile() {
     const name = document.getElementById("name-input").value
     const email = document.getElementById("email-input").value
     const interest = document.getElementById("interest-input").value
-    document.getElementById("name").innerText = name
-    document.getElementById("email").innerText = email
-    document.getElementById("interest").innerText = interest
+    document.getElementById("name").textContent = name
+    document.getElementById("email").textContent = email
+    document.getElementById("interest").textContent = interest
 
 
 
